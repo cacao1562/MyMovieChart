36a6f29c2b4e8680b66d44d1f6bb697e6be9d4b1
hwan Yu <hwanungyu@hwanui-MacBook-Pro.local>
2017-08-17 12:17:12 +0900

    Initial Commit

---
+++ MyMovieChart/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyMovieChart
+//
+//  Created by hwan ung Yu on 2017. 8. 17..
+//  Copyright © 2017년 hwan ung Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
